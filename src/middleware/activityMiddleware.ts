40fef8c09e558985996279a01c329ed5d63237db
HasanSbaih <H.sbaih93@gmail.com>
2024-01-31 18:32:53 +0300

    adding to dos

---
+++ src/middleware/activityMiddleware.ts
@@ -0,0 +1,6 @@
+
+//TODO Activcty tracking middleware
+function trackActivity() {
+  
+}
+export default trackActivity;
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -9,6 +9,6 @@ router.get("/projects/:projectId/tasks/", verifySession(), checkPermissions(1), 
 router.get("/projects/:projectId/tasks/:id", verifySession(), checkPermissions(1), taskController.getTask);
 router.post("/projects/:projectId/tasks/", verifySession(), checkPermissions(2), taskController.createTask);
 router.delete("/projects/:projectId/tasks/:id", verifySession(), checkPermissions(2), taskController.deleteTask);
-router.put("/projects/:projectId/tasks/:id", verifySession(), checkPermissions(2), taskController.updateTask);
+router.put("/projects/:projectId/tasks/:id", verifySession(), checkPermissions(2)/*TODO Activity*/, taskController.updateTask);
 
 export default router;
