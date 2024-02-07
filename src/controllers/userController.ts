29397d5efe782c214dbec8659ac341fb679d2e23
HasanSbaih <H.sbaih93@gmail.com>
2024-02-07 23:31:58 +0300

    file upload

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,6 +3,18 @@ import { Response } from "express";
 import { SessionRequest } from "supertokens-node/framework/express";
 import supertokens from "supertokens-node";
 import UserData from "../models/userData";
+import multer from 'multer';
+import { BlobServiceClient } from '@azure/storage-blob';
+
+
+const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING || "your_connection_string_here";
+const containerName = 'profile-pictures';
+const upload = multer({ dest: 'uploads/' });
+
+const crypto = require('crypto');
+
+
+
 
 export const getUserDetails = async (req: SessionRequest, res: Response) => {
     // Retrieve the userId from the session
@@ -17,15 +29,15 @@ export const getUserDetails = async (req: SessionRequest, res: Response) => {
 
     const userData = await UserData.findOne({
         userId: userId,
-      })
+    })
 
     return res.json({
         email: userInfo.emails,
-        name: userData!.name, 
+        name: userData!.name,
         profilePicture: userData?.profilePicture,
         timeJoineds: userInfo.timeJoined
-   });
-    
+    });
+
 };
 
 export const editUserDetails = async (req: SessionRequest, res: Response) => {
@@ -33,19 +45,57 @@ export const editUserDetails = async (req: SessionRequest, res: Response) => {
     const userId = req.session!.getUserId();
 
     // Construct the update object with inline type assertion
-    const update: { name?: string; profilePicture?: string; } = {};
+    const update: { name?: string; } = {};
     if (req.body.name) {
         update.name = req.body.name;
     }
-    if (req.body.profilePicture) {
-        update.profilePicture = req.body.profilePicture;
-    }
 
-    const user = await UserData.findOneAndUpdate({ userId: userId}, update);
-    
+    const user = await UserData.findOneAndUpdate({ userId: userId }, update);
+
     if (!user) {
         return res.status(404).send("User not found");
     }
 
     return res.json(user);
 };
+
+
+export const uploadProfilePicture = async (req: SessionRequest, res: Response) => {
+    // Retrieve the userId from the session
+    const userId = req.session!.getUserId();
+
+
+    // Construct the update object with inline type assertion
+    const update: { profilePicture?: string; } = {};
+
+
+    const user = await UserData.findOneAndUpdate({ userId: userId }, update);
+
+    if (!user) {
+        return res.status(404).send("User not found");
+    }
+
+    return res.json(user);
+};
+
+
+const uploadProfilePictureUsingAzure = async (userId: string) => {
+    try {
+        const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const hashedUserID = hashUserID(userId);
+        const blobName = `profile_${hashedUserID}.png`;
+
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+        // Upload the file
+        await blockBlobClient.uploadFile("picture/");
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function hashUserID(userID: string) {
+    return crypto.createHash('sha256').update(userID).digest('hex');
+}
